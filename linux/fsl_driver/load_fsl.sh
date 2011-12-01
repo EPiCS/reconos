5588c1f6baa32d0c76e8f16900668b53eb96457b
Andreas Agne <agne@upb.de>
2011-12-01 13:06:31 +0100

    FSL driver cleanup

--- linux/fsl_driver/load_fsl.sh
+++ linux/fsl_driver/load_fsl.sh
@@ -1,24 +1,43 @@
 #!/bin/sh
 
 MODULE="fsl"
-
 DEVICE="fsl"
+
+INTERRUPT_LIST=0,1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+INTERRUPTS=$(echo $INTERRUPT_LIST | tr ',' ' ')
 
 # invoke insmod with all arguments we got
 # and use a pathname, as newer modutils don't look in . by default
 
-rmmod $MODULE.ko 
-/sbin/insmod ./$MODULE.ko $* || exit 1
+mkdir -p /lib/modules/$(uname -r)
+cp /fsl.ko /lib/modules/$(uname -r)
+
+echo "removing module"
+rmmod $MODULE
+
+echo "inserting module"
+modprobe $MODULE fsl_interrupts=$INTERRUPT_LIST
 
 # remove stale nodes
+echo "removing stale device nodes"
 rm -f /dev/$DEVICE*
 
 MAJOR=$(grep $DEVICE /proc/devices)
-MAJORF=${MAJOR% $DEVICE}
+MAJOR=${MAJOR% $DEVICE}
 
-for n in 0 1
+echo "MAJOR=$MAJOR"
+
+i=0
+for n in $INTERRUPTS
 do
-	mknod /dev/$DEVICE$n c $MAJOR $n
+	if [ $n = -1 ]
+	then
+		i=$(expr $i + 1)
+		continue
+	fi
+	echo "creating device node /dev/$DEVICE$i"
+	mknod /dev/$DEVICE$i c $MAJOR $i
+	i=$(expr $i + 1)
 done
 
 # give appropriate group/permissions, and change the group.
