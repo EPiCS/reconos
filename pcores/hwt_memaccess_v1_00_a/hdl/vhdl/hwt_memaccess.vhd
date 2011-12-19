7c518a47211e8a366e49a408ce84f06f6d07707d
Andreas Agne <agne@upb.de>
2011-12-19 01:05:40 +0100

    * Made HWT more versatile * Removed devl directory

--- pcores/hwt_memaccess_v1_00_a/hdl/vhdl/hwt_memaccess.vhd
+++ pcores/hwt_memaccess_v1_00_a/hdl/vhdl/hwt_memaccess.vhd
@@ -39,15 +39,18 @@ entity hwt_memaccess is
 end hwt_memaccess;
 
 architecture implementation of hwt_memaccess is
-	type STATE_TYPE is (STATE_GET, STATE_READ, STATE_PUT);
+	type STATE_TYPE is (STATE_GET_REQUEST,STATE_GET_ADDR,STATE_READ_REQ,STATE_WRITE_REQ,STATE_READ,STATE_MBOX_GET,STATE_MBOX_PUT,STATE_WRITE);
 
 	type LOCAL_MEMORY_T is array (0 to 1023) of std_logic_vector(31 downto 0);
 	
 	constant MBOX_RECV  : std_logic_vector(C_FSL_WIDTH-1 downto 0) := x"00000000";
 	constant MBOX_SEND  : std_logic_vector(C_FSL_WIDTH-1 downto 0) := x"00000001";
 
-	signal addr     : std_logic_Vector(C_FSL_WIDTH-1 downto 0);
-	signal data     : std_logic_Vector(C_FSL_WIDTH-1 downto 0);
+	signal addr     : std_logic_vector(31 downto 0);
+	signal data     : std_logic_vector(31 downto 0);
+	signal req      : std_logic_vector(31 downto 0);
+	signal len      : std_logic_vector(23 downto 0);
+	signal cmd      : std_logic_vector(7 downto 0);
 	signal state  : STATE_TYPE;
 	signal osif     : fsl_t;
 	signal memif    : memif_t;
@@ -81,6 +84,8 @@ begin
 		FIFO32_M_Wr
 	);
 	
+	cmd <= req(31 downto 24);
+
 	-- os and memory synchronisation state machine
 	process (osif.clk) is
 		variable done : boolean;
@@ -88,23 +93,72 @@ begin
 		if rst = '1' then
 			osif_reset(osif);
 			memif_reset(memif);
-			state <= STATE_GET;
+			state <= STATE_GET_REQUEST;
 			done := False;
 			addr <= (others => '0');
 			data <= x"AFFE7654";
+			req <= (others => '0');
+			len <= (others => '0');
+			req <= (others => '0');
 		elsif rising_edge(osif.clk) then
 			case state is
-				when STATE_GET =>
+				when STATE_GET_REQUEST =>
+					osif_mbox_get(osif, MBOX_RECV, req, done);
+					if done then state <= STATE_GET_ADDR; end if;
+	
+				when STATE_GET_ADDR =>
 					osif_mbox_get(osif, MBOX_RECV, addr, done);
+					if done then
+						len <= req(23 downto 2) & "00";
+						addr <= addr(31 downto 2) & "00";
+						if cmd = MEMIF_CMD_READ then
+							state <= STATE_READ_REQ;
+						else
+							state <= STATE_WRITE_REQ;
+						end if;
+					end if;
+						
+				when STATE_READ_REQ =>
+					memif_read_request(memif,addr,len,done);
 					if done then state <= STATE_READ; end if;
+
+				when STATE_WRITE_REQ =>
+					memif_write_request(memif,addr,len,done);
+					if done then state <= STATE_MBOX_GET; end if;
 					
 				when STATE_READ =>
-					memif_read(memif, addr, data, done);
-					if done then state <= STATE_PUT; end if;
-					
-				when STATE_PUT =>
+					memif_fifo_pull(memif,data,done);
+					if done then
+						state <= STATE_MBOX_PUT;
+						len <= len - 4;
+					end if;
+
+				when STATE_MBOX_PUT =>
 					osif_mbox_put(osif, MBOX_SEND, data, ignore, done);
-					if done then state <= STATE_GET; end if;
+					if done then
+						if len = 0 then
+							state <= STATE_GET_REQUEST;
+						else
+							state <= STATE_READ;
+						end if;
+					end if;
+					
+				when STATE_MBOX_GET =>
+					osif_mbox_get(osif, MBOX_RECV, data, done);
+					if done then
+						state <= STATE_WRITE;
+						len <= len - 4;
+					end if;
+
+				when STATE_WRITE =>
+					memif_fifo_push(memif, data, done);
+					if done then
+						if len = 0 then
+							state <= STATE_GET_REQUEST;
+						else
+							state <= STATE_MBOX_GET;
+						end if;
+					end if;
 			end case;
 		end if;
 	end process;
