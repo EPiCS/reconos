8b727e52adfa6bef877e9a20943ce7fe388ec1dc
Andreas Agne <agne@upb.de>
2012-01-27 13:42:23 +0100

    * Moved semaphore test to new toolflow

---
+++ demos/semaphore/hw/setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+BASE_DESIGN="ml605_linux_13.3"
+HWTS="hwt_semaphore_test_v1_00_a"
+
+
+if [ -z "$RECONOS" ]
+then
+	echo "ERROR: RECONOS environment variable is not set. $RECONOS must point to the root of the ReconOS repository."
+	exit 1
+fi
+
+
+# copy base design
+cp -r $RECONOS/designs/$BASE_DESIGN edk
+
+# link to ReconOS pcores
+mkdir edk/pcores
+ln -s $RECONOS/pcores/* edk/pcores
+
+# add links to hardware threads
+cd edk/pcores
+for HWT in $HWTS
+do
+	ln -sf ../../$HWT .
+done
+cd -
+
+# add ReconOS to the edk project
+cp edk/system.mhs edk/system.mhs.orig
+$RECONOS/tools/mhsaddhwts.py edk/system.mhs.orig $HWTS > edk/system.mhs
+
+
