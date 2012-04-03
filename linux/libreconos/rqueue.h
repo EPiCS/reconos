c079552fc9686b80273c753f1b1e9fb6130fca4d
Daniel Borkmann <borkmann@iogearbox.net>
2012-04-03 17:16:50 +0200

    rqueue: minor: fixed function signature
    Signed-off-by: Daniel Borkmann <borkmann@iogearbox.net>

--- linux/libreconos/rqueue.h
+++ linux/libreconos/rqueue.h
@@ -13,7 +13,7 @@ typedef struct mbox rqueue;
 
 extern int rq_init(rqueue *rq, size_t size);
 extern void rq_close(rqueue *rq);
-extern int rq_receive(rqueue *rq, uint32_t *msg, size_t size);
+extern ssize_t rq_receive(rqueue *rq, uint32_t *msg, size_t size);
 extern void rq_send(rqueue *rq, uint32_t *msg, size_t size);
 
 #endif /* RQUEUE_H */
