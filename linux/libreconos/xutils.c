f54ce47c5f66e5f328cc571623180736c8df38fb
Daniel Borkmann <borkmann@iogearbox.net>
2012-04-03 15:42:44 +0200

    xutils: added xutils helper routines
    Signed-off-by: Daniel Borkmann <borkmann@iogearbox.net>

---
+++ linux/libreconos/xutils.c
@@ -0,0 +1,38 @@
+#include <string.h>
+#include <stdarg.h>
+#include <ctype.h>
+
+size_t strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t ret = strlen(src);
+
+	if (size) {
+		size_t len = (ret >= size) ? size - 1 : ret;
+
+		memcpy(dest, src, len);
+		dest[len] = '\0';
+	}
+
+	return ret;
+}
+
+int slprintf(char *dst, size_t size, const char *fmt, ...)
+{
+	int ret;
+	va_list ap;
+
+	va_start(ap, fmt);
+	ret = vsnprintf(dst, size, fmt, ap);
+	dst[size - 1] = '\0';
+
+	va_end(ap);
+	return ret;
+}
+
+int open_or_die(const char *file, int flags)
+{
+	int ret = open(file, flags);
+	if (ret < 0)
+		panic("Cannot open file %s!\n", file);
+	return ret;
+}
---
+++ linux/libreconos/xutils.h
@@ -0,0 +1,49 @@
+#ifndef XUTILS_H
+#define XUTILS_H
+
+#ifndef array_size
+# define array_size(x)		(sizeof(x) / sizeof((x)[0]) + __must_be_array(x))
+#endif
+
+#ifndef __must_be_array
+# define __must_be_array(x)		\
+	build_bug_on_zero(__builtin_types_compatible_p(typeof(x), typeof(&x[0])))
+#endif
+
+#ifndef build_bug_on_zero
+# define build_bug_on_zero(e)	(sizeof(char[1 - 2 * !!(e)]) - 1)
+#endif
+
+size_t strlcpy(char *dest, const char *src, size_t size);
+int slprintf(char *dst, size_t size, const char *fmt, ...);
+int open_or_die(const char *file, int flags);
+
+static inline void die(void)
+{
+	exit(EXIT_FAILURE);
+}
+
+static inline void panic(char *msg, ...)
+{
+	va_list vl;
+
+	va_start(vl, msg);
+	vfprintf(stderr, msg, vl);
+	va_end(vl);
+
+	fflush(stderr);
+	die();
+}
+
+static inline void whine(char *msg, ...)
+{
+	va_list vl;
+
+	va_start(vl, msg);
+	vfprintf(stderr, msg, vl);
+	va_end(vl);
+
+	fflush(stderr);
+}
+
+#endif /* XUTILS_H */
