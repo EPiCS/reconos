1d913a93c73bde1fbad05815f04a32805b483bb6
Daniel Borkmann <borkmann@iogearbox.net>
2012-04-25 16:35:26 +0200

    builtin_client: connect to server
    Signed-off-by: Daniel Borkmann <borkmann@iogearbox.net>

--- linux/tools/sensord/builtin_client.c
+++ linux/tools/sensord/builtin_client.c
@@ -2,7 +2,35 @@
  * Copyright 2012 Daniel Borkmann <dborkma@tik.ee.ethz.ch>
  */
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include "xutils.h"
+
+#define SOCK_ADDR	"sensordsock"
+
 int main(void)
 {
+	int sock, ret;
+	struct sockaddr_un saddr;
+	socklen_t slen;
+
+	sock = socket(AF_UNIX, SOCK_STREAM, 0);
+	if (sock < 0)
+		panic("Cannot create socket!\n");
+
+	memset(&saddr, 0, sizeof(saddr));
+	saddr.sun_family = AF_UNIX;
+	strncpy(saddr.sun_path, SOCK_ADDR, sizeof(saddr.sun_path));
+
+	slen = sizeof(saddr);
+	ret = connect(sock, (struct sockaddr *) &saddr, slen);
+
+	printf("Connected!\n");
+
+	close(sock);
 	return 0;
 }
