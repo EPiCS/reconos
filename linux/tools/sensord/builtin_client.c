413a28cd3118619150d98f40705cc58dce435abf
Daniel Borkmann <borkmann@iogearbox.net>
2012-04-25 16:40:17 +0200

    builtin_client: registered event exception handler
    Signed-off-by: Daniel Borkmann <borkmann@iogearbox.net>

--- linux/tools/sensord/builtin_client.c
+++ linux/tools/sensord/builtin_client.c
@@ -7,16 +7,36 @@
 #include <sys/un.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <signal.h>
 
 #include "xutils.h"
 
 #define SOCK_ADDR	"sensordsock"
+
+static void upper_threshold_triggered(int num)
+{
+	if (num != SIGUSR1)
+		return;
+
+	printf("ALERT: upper threshold triggered!\n");
+}
+
+static void lower_threshold_triggered(int num)
+{
+	if (num != SIGUSR2)
+		return;
+
+	printf("ALERT: lower threshold triggered!\n");
+}
 
 int main(void)
 {
 	int sock, ret;
 	struct sockaddr_un saddr;
 	socklen_t slen;
+
+	signal(SIGUSR1, upper_threshold_triggered);
+	signal(SIGUSR2, lower_threshold_triggered);
 
 	sock = socket(AF_UNIX, SOCK_STREAM, 0);
 	if (sock < 0)
