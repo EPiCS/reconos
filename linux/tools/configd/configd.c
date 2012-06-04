126116cf78dc2e380553a557aef4bc0eb3a50f5c
Daniel Borkmann <borkmann@iogearbox.net>
2012-06-04 15:55:51 +0200

    configd: detect the need for reliability
    Signed-off-by: Daniel Borkmann <borkmann@iogearbox.net>

--- linux/tools/configd/configd.c
+++ linux/tools/configd/configd.c
@@ -15,7 +15,12 @@
 #include <stdint.h>
 #include <signal.h>
 #include <errno.h>
+#include <linux/netlink.h>
+#include <linux/types.h>
+#include <linux/if.h>
 
+#include "xt_vlink.h"
+#include "xt_fblock.h"
 #include "xutils.h"
 #include "notification.h"
 
@@ -23,20 +28,84 @@
 
 static void *buffshared = NULL;
 
-static sig_atomic_t need_reliability = 0;
+static sig_atomic_t need_reliability = 0, need_reliability_switched = 0;
+
+static void send_netlink(struct lananlmsg *lmsg)
+{
+	int sock, ret;
+	struct sockaddr_nl src_addr, dest_addr;
+	struct nlmsghdr *nlh;
+	struct iovec iov;
+	struct msghdr msg;
+
+	if (unlikely(!lmsg))
+		return;
+
+	sock = socket(PF_NETLINK, SOCK_RAW, NETLINK_USERCTL);
+	if (unlikely(sock < 0))
+		panic("Cannot get NETLINK_USERCTL socket from kernel! "
+		      "Modules not loaded?!\n");
+
+	memset(&src_addr, 0, sizeof(src_addr));
+	src_addr.nl_family = AF_NETLINK;
+	src_addr.nl_pad = 0;
+	src_addr.nl_pid = getpid();
+	src_addr.nl_groups = 0;
+
+	ret = bind(sock, (struct sockaddr *) &src_addr, sizeof(src_addr));
+	if (unlikely(ret))
+		panic("Cannot bind socket!\n");
+
+	memset(&dest_addr, 0, sizeof(dest_addr));
+	dest_addr.nl_family = AF_NETLINK;
+	dest_addr.nl_pad = 0;
+	dest_addr.nl_pid = 0;
+	dest_addr.nl_groups = 0;
+
+	nlh = xzmalloc(NLMSG_SPACE(sizeof(*lmsg)));
+	nlh->nlmsg_len = NLMSG_SPACE(sizeof(*lmsg));
+	nlh->nlmsg_pid = getpid();
+	nlh->nlmsg_type = USERCTLGRP_CONF;
+	nlh->nlmsg_flags = NLM_F_REQUEST;
+
+	memcpy(NLMSG_DATA(nlh), lmsg, sizeof(*lmsg));
+
+	iov.iov_base = nlh;
+	iov.iov_len = nlh->nlmsg_len;
+
+	memset(&msg, 0, sizeof(msg));
+	msg.msg_name = &dest_addr;
+	msg.msg_namelen = sizeof(dest_addr);
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+
+	ret = sendmsg(sock, &msg, 0);
+	if (unlikely(ret < 0))
+		panic("Cannot send NETLINK message to the kernel!\n");
+
+	close(sock);
+	xfree(nlh);
+}
 
 static void reconfig_reliability(void)
 {
-	if (need_reliability)
+	if (need_reliability == 1 && need_reliability_switched == 1) {
 		printf("Need reliability!\n");
-	else
+		/* ... include reliability */
+	} else if (need_reliability == 0 && need_reliability_switched == 1) {
 		printf("Don't need reliability!\n");
+		/* ... exclude reliability */
+	}
+
+	need_reliability_switched = 0;
 }
 
 static void upper_threshold_triggered(int num)
 {
 	if (num != SIGUSR1)
 		return;
+	if (need_reliability == 1)
+		need_reliability_switched = 1;
 	need_reliability = 0;
 }
 
@@ -44,6 +113,8 @@ static void lower_threshold_triggered(int num)
 {
 	if (num != SIGUSR2)
 		return;
+	if (need_reliability == 0)
+		need_reliability_switched = 1;
 	need_reliability = 1;
 }
 
