90f183b7e05b7a860c3ff79f3eecb41afa292808
Daniel Borkmann <borkmann@iogearbox.net>
2012-09-03 17:24:50 +0200

    configd: add ping
    Signed-off-by: Daniel Borkmann <borkmann@iogearbox.net>

---
+++ linux/tools/configd/builtin_ping.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/ioctl.h>
+#include <linux/netlink.h>
+#include <linux/types.h>
+#include <linux/if.h>
+
+#include "xt_vlink.h"
+#include "xt_fblock.h"
+#include "xutils.h"
+
+#define SOCK_ADDR	"/tmp/configdsock"
+
+struct bind_msg {
+	char name[FBNAMSIZ];
+	char app[FBNAMSIZ];
+	enum fblock_props props[MAX_PROPS];
+	int flags;
+};
+
+static int bind_config(struct bind_msg *bmsg)
+{
+	int sock;
+	ssize_t ret;
+	struct sockaddr_un saddr;
+	socklen_t slen;
+
+	sock = socket(AF_UNIX, SOCK_STREAM, 0);
+	if (sock < 0)
+		panic("Cannot create socket!\n");
+
+	memset(&saddr, 0, sizeof(saddr));
+	saddr.sun_family = AF_UNIX;
+	strncpy(saddr.sun_path, SOCK_ADDR, sizeof(saddr.sun_path));
+
+	slen = sizeof(saddr);
+	ret = connect(sock, (struct sockaddr *) &saddr, slen);
+	if (ret < 0)
+		panic("Cannot connect to server!\n");
+
+	ret = write(sock, bmsg, sizeof(*bmsg));
+	if (ret <= 0)
+		panic("Cannot write to server: %s\n", strerror(errno));
+
+	close(sock);
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	int sock, ret, i, client = -1;
+	char buff[512];
+	struct bind_msg *bmsg;
+
+	if (argc!=2)
+		panic("usage: %s <client|server>\n", argv[0]);
+
+	if (!strncmp("client", argv[1], strlen("client")))
+		client = 1;
+	else if (!strncmp("server", argv[1], strlen("server")))
+		client = 0;
+	else
+		panic("usage: %s <client|server>\n", argv[0]);
+
+	sock = socket(27, SOCK_RAW, 0);
+	if (sock < 0)
+		panic("Cannot create socket!\n");
+
+	memset(buff, 0, sizeof(buff));
+	bmsg = (struct bind_msg *) buff;
+	if (client) {
+		bmsg->props[0] = DUMMY;
+		strcpy(bmsg->app, "ping");
+		bmsg->flags = TYPE_CLIENT;
+	} else {
+		strcpy(bmsg->app, "ping");
+		bmsg->flags = TYPE_SERVER;
+	}
+
+	ret = ioctl(sock, 35296, bmsg->name);
+	if (ret < 0)
+		panic("Cannot do ioctl!\n");
+
+	ret = bind_config(bmsg);
+	if (ret < 0)
+		panic("Cannot bind configuration!\n");
+
+	printf("Config bound!\n");
+
+	memset(buff, 0xff, sizeof(buff));
+	while (1) {
+		if (client) {
+			ret = sendto(sock, buff, 64, 0, NULL, 0);
+			printf("Sent %d bytes: ", ret);
+			for (i = 0; i < ret; ++i) {
+				printf("%2x ", buff[i]);
+			}
+			printf("\n");
+		} else {
+			ret = recvfrom(sock, buff, sizeof(buff), 0, NULL, 0);
+			printf("Received %d bytes: ", ret);
+			for (i = 0; i < ret; ++i) {
+				printf("%2x ", buff[i]);
+			}
+			printf("\n");
+		}
+		sleep(1);
+	}
+
+	close(sock);
+	return 0;
+}
