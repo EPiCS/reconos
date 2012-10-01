682b3849f8d2f091c7a68fa803d39898bd5ef831
Daniel Borkmann <borkmann@iogearbox.net>
2012-10-01 16:30:08 +0200

    clenaup irr
    Signed-off-by: Daniel Borkmann <borkmann@iogearbox.net>

--- linux/net/fb_irr.c
+++ linux/net/fb_irr.c
@@ -222,7 +222,15 @@ err:
 
 static void fb_irr_dtor(struct fblock *fb)
 {
-	kfree(rcu_dereference_raw(fb->private_data));
+	struct fb_irr_priv *fb_priv;
+
+	fb_priv = rcu_dereference_raw(fb->private_data);
+
+	tasklet_hrtimer_cancel(&fb_priv->htimer);
+
+	kfree(fb_priv->hold);
+	kfree(fb_priv);
+
 	module_put(THIS_MODULE);
 }
 
