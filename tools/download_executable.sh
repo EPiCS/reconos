6bbe6484c78b73e4acae2cc9c4d884cfc223e4db
Sebastian Meisner <meise@upb.de>
2013-05-22 13:18:02 +0200

    - extending download tools to be able to download elf files to slave processor

--- tools/download_executable.sh
+++ tools/download_executable.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -i
+
+# Called from $RECONOS/tools/dow
+# Parameters: download_executable.sh <elf file> [cpu nr]
+
 LAST_ELF_FILE="/tmp/.xmd_download_last"
 
 # FIXME: default architecture is microblaze
@@ -29,12 +33,22 @@ fi
 if [ "$ARCH" = "ppc" ]; then
     DOWCMD="ppccon\ndow $ELF\nrun\n"
 elif [ "$ARCH" = "mb" ]; then
-    DOWCMD="connect mb mdm\ndow $ELF\nrun\n"
+    if [ -z $2 ]; then 
+        echo "Programming master CPU."
+        DOWCMD="connect mb mdm\ndow $ELF\nrun\n"
+    else
+        echo "Programming slave CPU nr. $2."
+        DOWCMD="connect mb mdm -debugdevice cpunr $2 \n\
+ debugconfig -reset_on_data_dow processor enable \n\
+ debugconfig -reset_on_run processor enable \n\
+ rst -processor \n\
+ dow $ELF \n\
+ run \n"
+    fi      
 else
     echo "unknown ARCH '$ARCH'"
     exit 1
 fi
-
 
 if [ -e "$XILINX_EDK/bin/lin64/xmd" ] ; then
         echo -e $DOWCMD | $XILINX_EDK/bin/lin64/xmd
