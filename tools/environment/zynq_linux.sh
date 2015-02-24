fb7632954004fdbc794aa9ab3f76c7f25b89e23d
Christoph Rüthing <ruething@mail.upb.de>
2015-02-24 10:21:35 +0100

    Removed old environment settings

--- tools/environment/zynq_linux.sh
+++
@@ -1,9 +0,0 @@
-xil_tools="/opt/Xilinx/14.7"
-gnutools="/opt/Xilinx/14.7/ISE_DS/EDK/gnu/arm/lin/bin/arm-xilinx-linux-gnueabi-"
-reconos_arch="zynq"
-reconos_os="linux"
-reconos_mmu=true
-
-export KDIR=/home/christoph/Documents/reconos/zynq/linux-xlnx
-export PATH=/home/christoph/Documents/reconos/zynq/u-boot-xlnx/tools:$PATH
-
