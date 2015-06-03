3da2dc8adc67c8dc7b7bd30df3b893dff65b8f6c
Sebastian Meisner <meise@upb.de>
2015-06-03 16:49:23 +0200

    - added support  for matrixmul runs

--- demos/sort_demo_shadowed_mem/linux/sh_mem_bench_automated.py
+++ demos/sort_demo_shadowed_mem/linux/sh_mem_bench_automated.py
@@ -4,44 +4,79 @@ import sys, re, string, pexpect, subprocess, getpass, time, datetime
 from packagekit.enums import EXIT_SUCCESS
 
 TIMEOUT_SEC = 300
-
-RECONOS= "/home/meise/git/reconos_epics/"
-DOW = RECONOS+"tools/dow"
-KERNEL = "/home/meise/git/linux-2.6-xlnx/arch/microblaze/boot/simpleImage.ml605_epics"
-BIT_SORT = RECONOS+"demos/sort_demo_shadowed_mem/hw/edk/implementation/system.bit"
-BIT_SORT_REL = RECONOS+"demos/sort_demo_shadowed_mem/hw/edk_mem_rel/implementation/system.bit"
-BIT_SORT_PERF = RECONOS+"demos/sort_demo_shadowed_mem/hw/edk_mem_perf/implementation/system.bit"
-DEMO_EXPORT_DIR="/exports/rootfs_mb/demos/sort_demo_shadowed_mem/"
-
 REPEAT_COUNT = 3
-PARAMS = [4,8,16,32,64,128,256,512]
+
+EXPORT_DIR= "/exports/rootfs_mb"
+RECONOS= "/home/meise/git/reconos_epics"
+DOW = RECONOS+"/tools/dow"
+KERNEL = "/home/meise/git/linux-2.6-xlnx/arch/microblaze/boot/simpleImage.ml605_epics"
 
 LOG_FILE_BASELINE="baseline_run"
 LOG_FILE_BASELINE_SH="baseline_sh_run"
 LOG_FILE_BASELINE_SH_ON_LVL1="baseline_sh_on_lvl1_run"
 LOG_FILE_BASELINE_SH_ON_LVL2="baseline_sh_on_lvl2_run"
 LOG_FILE_BASELINE_SH_ON_LVL3="baseline_sh_on_lvl3_run" # makes no sense for baseline, do it for sanity checking
-commands_baseline = ["./sort_demo -h 1 -s 0 -b "+str(x)+" >> {}/"+LOG_FILE_BASELINE+" 2>&1" for x in PARAMS]
-commands_baseline += ["./sort_demo_shadowed -h 1 -s 0 -b "+str(x)+" >> {}/"+LOG_FILE_BASELINE_SH+" 2>&1" for x in PARAMS]
-commands_baseline += ["./sort_demo_shadowed -h 1 -s 0 -b "+str(x)+" -a --level 1 >> {}/"+LOG_FILE_BASELINE_SH_ON_LVL1+" 2>&1" for x in PARAMS]
-commands_baseline += ["./sort_demo_shadowed -h 1 -s 0 -b "+str(x)+" -a --level 2 >> {}/"+LOG_FILE_BASELINE_SH_ON_LVL2+" 2>&1" for x in PARAMS]
-commands_baseline += ["./sort_demo_shadowed -h 1 -s 0 -b "+str(x)+" -a --level 3 >> {}/"+LOG_FILE_BASELINE_SH_ON_LVL3+" 2>&1" for x in PARAMS]
-
-#LOG_FILE_REL_OFF="rel_off_run"
 LOG_FILE_REL_ON_LVL1="rel_on_lvl1_run"
 LOG_FILE_REL_ON_LVL2="rel_on_lvl2_run"
 LOG_FILE_REL_ON_LVL3="rel_on_lvl3_run"
-commands_rel = ["./sort_demo_shadowed -h 1 -s 0 -b "+str(x)+" -a  --level 1 >> {}/"+LOG_FILE_REL_ON_LVL1 +" 2>&1" for x in PARAMS] 
-commands_rel +=["./sort_demo_shadowed -h 1 -s 0 -b "+str(y)+" -a  --level 2 >> {}/"+LOG_FILE_REL_ON_LVL2 +" 2>&1" for y in PARAMS]
-commands_rel +=["./sort_demo_shadowed -h 1 -s 0 -b "+str(y)+" -a  --level 3 >> {}/"+LOG_FILE_REL_ON_LVL3 +" 2>&1" for y in PARAMS]
-
-#LOG_FILE_PERF_OFF="perf_off_run"
 LOG_FILE_PERF_ON_LVL1="perf_on_lvl1_run"
 LOG_FILE_PERF_ON_LVL2="perf_on_lvl2_run"
 LOG_FILE_PERF_ON_LVL3="perf_on_lvl3_run"
-commands_perf = ["./sort_demo_shadowed -h 1 -s 0 -b "+str(x)+" -a  --level 1 >> {}/"+LOG_FILE_PERF_ON_LVL1 +" 2>&1" for x in PARAMS] 
-commands_perf +=["./sort_demo_shadowed -h 1 -s 0 -b "+str(y)+" -a  --level 2 >> {}/"+LOG_FILE_PERF_ON_LVL2 +" 2>&1" for y in PARAMS]
-commands_perf +=["./sort_demo_shadowed -h 1 -s 0 -b "+str(y)+" -a  --level 3 >> {}/"+LOG_FILE_PERF_ON_LVL3 +" 2>&1" for y in PARAMS]
+
+
+BIT_SORT = RECONOS+"/demos/sort_demo_shadowed_mem/hw/edk/implementation/system.bit"
+BIT_SORT_REL = RECONOS+"/demos/sort_demo_shadowed_mem/hw/edk_mem_rel/implementation/system.bit"
+#BIT_SORT_PERF = RECONOS+"/demos/sort_demo_shadowed_mem/hw/edk_mem_perf/implementation/system.bit"
+BIT_SORT_PERF = "/home/meise/Desktop/system_arb_perf_8192.bit"
+
+SORT_DEMO_DIR="/demos/sort_demo_shadowed_mem"
+
+SORT_PARAMS = [4,8,16,32,64,128,256,512]
+
+sort_commands_baseline = ["./sort_demo -h 1 -s 0 -b "+str(x)+" >> {}/sort_"+LOG_FILE_BASELINE+" 2>&1" for x in SORT_PARAMS]
+sort_commands_baseline += ["./sort_demo_shadowed -h 1 -s 0 -b "+str(x)+" >> {}/sort_"+LOG_FILE_BASELINE_SH+" 2>&1" for x in SORT_PARAMS]
+sort_commands_baseline += ["./sort_demo_shadowed -h 1 -s 0 -b "+str(x)+" -a --level 1 >> {}/sort_"+LOG_FILE_BASELINE_SH_ON_LVL1+" 2>&1" for x in SORT_PARAMS]
+sort_commands_baseline += ["./sort_demo_shadowed -h 1 -s 0 -b "+str(x)+" -a --level 2 >> {}/sort_"+LOG_FILE_BASELINE_SH_ON_LVL2+" 2>&1" for x in SORT_PARAMS]
+sort_commands_baseline += ["./sort_demo_shadowed -h 1 -s 0 -b "+str(x)+" -a --level 3 >> {}/sort_"+LOG_FILE_BASELINE_SH_ON_LVL3+" 2>&1" for x in SORT_PARAMS]
+
+#LOG_FILE_REL_OFF="rel_off_run"
+
+sort_commands_rel = ["./sort_demo_shadowed -h 1 -s 0 -b "+str(x)+" -a  --level 1 >> {}/sort_"+LOG_FILE_REL_ON_LVL1 +" 2>&1" for x in SORT_PARAMS] 
+sort_commands_rel +=["./sort_demo_shadowed -h 1 -s 0 -b "+str(y)+" -a  --level 2 >> {}/sort_"+LOG_FILE_REL_ON_LVL2 +" 2>&1" for y in SORT_PARAMS]
+sort_commands_rel +=["./sort_demo_shadowed -h 1 -s 0 -b "+str(y)+" -a  --level 3 >> {}/sort_"+LOG_FILE_REL_ON_LVL3 +" 2>&1" for y in SORT_PARAMS]
+
+#LOG_FILE_PERF_OFF="perf_off_run"
+
+sort_commands_perf = ["./sort_demo_shadowed -h 1 -s 0 -b "+str(x)+" -a  --level 1 >> {}/sort_"+LOG_FILE_PERF_ON_LVL1 +" 2>&1" for x in SORT_PARAMS] 
+sort_commands_perf +=["./sort_demo_shadowed -h 1 -s 0 -b "+str(y)+" -a  --level 2 >> {}/sort_"+LOG_FILE_PERF_ON_LVL2 +" 2>&1" for y in SORT_PARAMS]
+sort_commands_perf +=["./sort_demo_shadowed -h 1 -s 0 -b "+str(y)+" -a  --level 3 >> {}/sort_"+LOG_FILE_PERF_ON_LVL3 +" 2>&1" for y in SORT_PARAMS]
+
+
+BIT_MATRIX = RECONOS+"/demos/matrixmul_shadowed_mem/hw/edk_linux/implementation/system.bit"
+BIT_MATRIX_REL = RECONOS+"/demos/matrixmul_shadowed_mem/hw/edk_linux_mem_rel/implementation/system.bit"
+BIT_MATRIX_PERF = RECONOS+"/demos/matrixmul_shadowed_mem/hw/edk_linux_mem_perf/implementation/system.bit"
+MATRIX_DEMO_DIR="/demos/matrixmul_shadowed_mem"
+
+MATRIX_PARAMS = [256,512] #,1024]
+
+matrix_commands_baseline = ["./matrixmul -f -h 1 -s 0 -b "+str(x)+" >> {}/matrix_"+LOG_FILE_BASELINE+" 2>&1" for x in MATRIX_PARAMS]
+matrix_commands_baseline += ["./matrixmul_shadowed -f -h 1 -s 0 -b "+str(x)+" >> {}/matrix_"+LOG_FILE_BASELINE_SH+" 2>&1" for x in MATRIX_PARAMS]
+matrix_commands_baseline += ["./matrixmul_shadowed -f -h 1 -s 0 -b "+str(x)+" -a --level 1 >> {}/matrix_"+LOG_FILE_BASELINE_SH_ON_LVL1+" 2>&1" for x in MATRIX_PARAMS]
+matrix_commands_baseline += ["./matrixmul_shadowed -f -h 1 -s 0 -b "+str(x)+" -a --level 2 >> {}/matrix_"+LOG_FILE_BASELINE_SH_ON_LVL2+" 2>&1" for x in MATRIX_PARAMS]
+matrix_commands_baseline += ["./matrixmul_shadowed -f -h 1 -s 0 -b "+str(x)+" -a --level 3 >> {}/matrix_"+LOG_FILE_BASELINE_SH_ON_LVL3+" 2>&1" for x in MATRIX_PARAMS]
+
+#LOG_FILE_REL_OFF="rel_off_run"
+
+matrix_commands_rel = ["./matrixmul_shadowed -f -h 1 -s 0 -b "+str(x)+" -a  --level 1 >> {}/matrix_"+LOG_FILE_REL_ON_LVL1 +" 2>&1" for x in MATRIX_PARAMS] 
+matrix_commands_rel +=["./matrixmul_shadowed -f -h 1 -s 0 -b "+str(y)+" -a  --level 2 >> {}/matrix_"+LOG_FILE_REL_ON_LVL2 +" 2>&1" for y in MATRIX_PARAMS]
+matrix_commands_rel +=["./matrixmul_shadowed -f -h 1 -s 0 -b "+str(y)+" -a  --level 3 >> {}/matrix_"+LOG_FILE_REL_ON_LVL3 +" 2>&1" for y in MATRIX_PARAMS]
+
+#LOG_FILE_PERF_OFF="perf_off_run"
+
+matrix_commands_perf = ["./matrixmul_shadowed -f -h 1 -s 0 -b "+str(x)+" -a  --level 1 >> {}/matrix_"+LOG_FILE_PERF_ON_LVL1 +" 2>&1" for x in MATRIX_PARAMS] 
+matrix_commands_perf +=["./matrixmul_shadowed -f -h 1 -s 0 -b "+str(y)+" -a  --level 2 >> {}/matrix_"+LOG_FILE_PERF_ON_LVL2 +" 2>&1" for y in MATRIX_PARAMS]
+matrix_commands_perf +=["./matrixmul_shadowed -f -h 1 -s 0 -b "+str(y)+" -a  --level 3 >> {}/matrix_"+LOG_FILE_PERF_ON_LVL3 +" 2>&1" for y in MATRIX_PARAMS]
+
 
 
 def downloadStuff(_bitstreamOrKernel):
@@ -66,7 +101,7 @@ class ReturnValueError(Exception):
     def __str__(self):
         return repr(self.value)
 
-def runBenchmark(_benchmarkTag, _telnetPasswd, _commands):
+def runBenchmark(_benchmarkTag, _telnetPasswd, _commands, _work_dir):
 
     # LOGIN
     child = pexpect.spawn ('telnet 192.168.35.2')
@@ -77,7 +112,7 @@ def runBenchmark(_benchmarkTag, _telnetPasswd, _commands):
     
     # Preparations
     child.expect('# ')
-    child.sendline('cd /demos/sort_demo_shadowed_mem')
+    child.sendline('cd '+ _work_dir)
     
     child.expect('# ')
     child.sendline('mkdir -p '+ _benchmarkTag)
@@ -144,7 +179,7 @@ def runBenchmark(_benchmarkTag, _telnetPasswd, _commands):
     child.sendline('exit')
     child.expect('Connection closed by foreign host.')
 
-def benchmark(_tagBase, _telnetPasswd, _bitstream, _commands):
+def benchmark(_tagBase, _telnetPasswd, _bitstream, _commands, _work_dir):
     print("Downloading Bitstream...")
     downloadStuff(_bitstream)
     print("Downloading Kernel...")
@@ -152,7 +187,7 @@ def benchmark(_tagBase, _telnetPasswd, _bitstream, _commands):
     print("Waiting for system boot...")
     time.sleep(60)
     print("Executing Benchmark...")
-    runBenchmark(_tagBase, _telnetPasswd, _commands)
+    runBenchmark(_tagBase, _telnetPasswd, _commands, _work_dir)
 
 if __name__ == "__main__":
     telnetPasswd = getpass.getpass('telnet password: ')
@@ -163,9 +198,13 @@ if __name__ == "__main__":
     # Benchmarks
     #
     
-    #benchmark(benchmarkTagBase, telnetPasswd, BIT_SORT,      commands_baseline)
-    #benchmark(benchmarkTagBase, telnetPasswd, BIT_SORT_REL,  commands_rel)
-    benchmark(benchmarkTagBase, telnetPasswd, BIT_SORT_PERF, commands_perf)
+    #benchmark(benchmarkTagBase, telnetPasswd, BIT_SORT,      sort_commands_baseline,   SORT_DEMO_DIR)
+    #benchmark(benchmarkTagBase, telnetPasswd, BIT_SORT_REL,  sort_commands_rel,        SORT_DEMO_DIR)
+    #benchmark(benchmarkTagBase, telnetPasswd, BIT_SORT_PERF, sort_commands_perf,        SORT_DEMO_DIR)
+    
+    benchmark(benchmarkTagBase, telnetPasswd, BIT_MATRIX,      matrix_commands_baseline,    MATRIX_DEMO_DIR)
+    benchmark(benchmarkTagBase, telnetPasswd, BIT_MATRIX_REL,  matrix_commands_rel,         MATRIX_DEMO_DIR)
+    benchmark(benchmarkTagBase, telnetPasswd, BIT_MATRIX_PERF, matrix_commands_perf,        MATRIX_DEMO_DIR)
     
     print("Done!")
     sys.exit(EXIT_SUCCESS)
