d366ef9d0fce02de58e74ca9a7bc559cffaeef71
Sebastian Meisner <meise@upb.de>
2015-07-27 13:42:38 +0200

    - checking in long forgotten setup file

---
+++ demos/sort_demo_shadowed/hw/setup_workercpu.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+BASE_DESIGN="ml605_linux_13.3"
+TARGET_DIR="edk_workercpu_mb"
+HWTS="hwt_workercpu_mb_v1_00_c"
+
+if [ -z "$RECONOS" ]
+then
+	echo "ERROR: RECONOS environment variable is not set. $RECONOS must point to the root of the ReconOS repository."
+	exit 1
+fi
+
+
+# copy base design
+cp -r $RECONOS/designs/$BASE_DESIGN $TARGET_DIR
+
+# link to ReconOS pcores
+mkdir $TARGET_DIR/pcores
+ln -s $RECONOS/pcores/* $TARGET_DIR/pcores
+
+# add links to hardware threads
+cd $TARGET_DIR/pcores
+for HWT in $HWTS
+do
+	ln -sf ../../$HWT .
+done
+cd -
+
+# add ReconOS to the edk project
+cp $TARGET_DIR/system.mhs $TARGET_DIR/system.mhs.orig
+$RECONOS/tools/mhsaddhwts.py $TARGET_DIR/system.mhs.orig $HWTS > $TARGET_DIR/system.mhs
+
+
