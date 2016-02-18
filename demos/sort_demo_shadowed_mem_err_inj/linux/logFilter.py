c0ddf1f6892526cb7566b32366afce5a8d3ec308
Sebastian Meisner <meise@upb.de>
2016-02-18 15:45:49 +0100

    - fixed some bugs: extraction of false error codes from *outputs file works now reliably   and update of summary file updates all 600 and some false error codes.

--- demos/sort_demo_shadowed_mem_err_inj/linux/logFilter.py
+++ demos/sort_demo_shadowed_mem_err_inj/linux/logFilter.py
@@ -225,10 +225,10 @@ def addressesToStrings(_addresses):
         stringList.append("{},{},{},{},{},{},{}".format(*a))
     return stringList
 
-def containsAddress(_line, _addressAsStrings):
-    for a,idx in zip(_addressAsStrings, xrange(len(_addressAsStrings))):
-        if a in _line: return True, idx
-    return False, -1
+#def containsAddress(_line, _addressAsStrings):
+#    for a,idx in zip(_addressAsStrings, xrange(len(_addressAsStrings))):
+#        if a in _line: return True, idx
+#    return False, -1
 
 progressIndicatorState=0
 def progressIndicator():
@@ -237,7 +237,7 @@ def progressIndicator():
     progressIndicatorState += 1
     
     if (progressIndicatorState%128 == 0):
-        sys.stdout.write( "\r{} {}".format(progress[(progressIndicatorState)%len(progress)],progressIndicatorState) ) #/100
+        sys.stdout.write( "\r{} {}".format(progress[(progressIndicatorState/128)%len(progress)],progressIndicatorState) ) #/100
         sys.stdout.flush()
     
 def extractFalseAddresses(_logfile):
@@ -254,17 +254,20 @@ def extractFalseAddresses(_logfile):
         #print(line, end="")
         progressIndicator()
         if state == "search":
-            ca, addressIdx = containsAddress(line, addressesAsStrings) 
-            if ca:
+            #ca, addressIdx = containsAddress(line, addressesAsStrings) 
+            if "./xilsem_err_inj -p"+addressesAsStrings[addressIdx]+"\n" in line:
                 state = "extract"
+                addressIdx += 1
+                if addressIdx >= len(addressesAsStrings): break
+                    #return falseErrorCodeList 
                 #print(line,end="")
                 
         elif state == "extract":
             #print(line,end="")
              if "SH_MEM_ERR" in line:
                  falseErrorCode=True
-             ca, dummy = containsAddress(line, ["###########"])
-             if ca:
+             #ca, dummy = containsAddress(line, ["###########"])
+             if "###########" in line:
                 state = "search"
                 if falseErrorCode:
                     falseErrorCode=False
@@ -293,7 +296,7 @@ Waiting for system boot...
     for line in _infile:
         
         # DEBUG
-        if i > 200:
+        if False and i > 200:
             print("")
             print("i: {}".format(i))
             print("Line: "+line)
@@ -308,14 +311,16 @@ Waiting for system boot...
         if state == "copy":
             _outfile.write(line)
             # Space at end is important: it makes sure that 1,2,3,4 does match only 1,2,3,4 and not 1,2,3,45 too.
-            if "FA "+_addresses[i]+" " in line:
+            if i < len(_addresses) and "FA "+_addresses[i]+" " in line:
             #if containsAddress(line, _addresses):
                 _outfile.write(sub)
                 state = "substitute" # skip lines until next "FA ..."
                 i += 1
+                #if i >= len(_addresses): break
                 lastLine = lineCnt
+    # DEBUG
     print("Last State: {}".format(state))
-    print("Last Address: {}".format(_addresses[i]))
+    print("Last Address: {}".format(_addresses[i-1]))
     print("Last Line: {}".format(lastLine))
     print("Linecount: {}".format(lineCnt))
     
@@ -330,16 +335,21 @@ def addressLessThan(a,b):
 if __name__ == '__main__':
     falseErrorCodeList = []
     
+    if len(sys.argv) < 4:
+        print("Too few arguments!")
+        sys.exit()
     
     with open(sys.argv[1]) as logfile:
         falseErrorCodeList = extractFalseAddresses(logfile)
-                    
+    print("")
+    print("Length of falseErrorCodes: {}".format(len(falseErrorCodeList)))
+    if len(falseErrorCodeList) == 0: sys.exit()
     
     # Results from previous run 
     #falseErrorCodeList = [0, 1, 2, 3, 4, 6, 7, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 57, 58, 59, 60, 61, 62, 63, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 209, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 226, 228, 229, 230, 230, 232, 233, 234, 235, 234, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 296, 297, 298, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 329, 330, 332, 333, 334, 335, 336, 337, 338, 339, 340, 341, 341, 343, 344, 343, 346, 347, 348, 349, 350, 351, 352, 353, 354, 355, 356, 357, 358, 359, 360, 361, 362, 363, 365, 366, 367, 368, 369, 370, 371, 372, 373, 371, 375, 376, 377, 378, 379, 380, 380, 382, 383, 384, 385, 387, 388, 389, 390, 391, 392, 393, 394, 395, 396, 397, 398, 400, 402, 403, 404, 405, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 421, 422, 423, 424, 425, 426, 425, 428, 429, 430, 431, 433, 434, 435, 436, 437, 438, 439, 440, 441, 442, 443, 444, 445, 446, 447, 448, 449, 450, 451, 452, 453, 454, 455, 456, 456, 456, 459, 460, 461, 462, 463, 464, 465, 466, 467, 468, 469, 470, 471, 472, 473, 474, 475, 476, 477, 480, 481, 482, 483, 485, 486, 487, 488, 489, 490, 491, 492, 493, 494, 495, 496, 497, 498, 499, 499, 499, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 512, 513, 514, 515, 516, 517, 518, 519, 520, 520, 522, 512, 514, 520, 523, 532, 533, 537, 539, 540, 543, 546, 550, 552, 556, 558, 559, 562, 565, 566, 570, 574, 578, 581, 583, 586, 588, 589, 591, 593, 595, 598, 609, 612, 615, 616, 624, 628, 633, 637, 643, 648, 616, 216, 324, 390, 429, 495, 534, 537, 552, 556, 628, 637, 667, 522, 523, 524, 525, 526, 527, 528, 529, 530, 531, 534, 535, 534, 535, 536, 535, 535, 532, 533, 534, 535, 536, 537, 538, 539, 540, 541, 542, 543, 544, 545, 546, 547, 548, 549, 550, 550, 552, 553, 554, 555, 556, 557, 558, 559, 560, 561, 562, 563, 564, 565, 566, 567, 568, 569, 570, 571, 572, 573, 574, 575, 576, 577, 578, 579, 580, 582, 583, 584, 585, 586, 587, 588, 589, 590, 591, 592, 593, 594, 595, 596, 597, 598, 599, 600, 601, 602, 603, 604, 605, 608, 609, 610, 611, 612, 614, 615, 616, 617, 618, 619, 620, 621, 620, 622, 623, 624, 625, 626, 627, 628, 629, 630, 631, 632, 633, 634, 635, 636, 637, 638, 639, 640, 641, 642, 643, 644, 643, 643, 647, 648, 649, 650, 651, 652, 648, 654, 655, 656, 656, 658, 659, 660, 661, 662, 663, 664, 665, 666, 667, 668, 669, 670, 671, 672, 673, 674, 675, 676, 677, 678, 681, 682, 682, 684, 685, 687, 688, 689, 690, 691, 692, 693, 694, 696, 698, 704, 695, 696, 697, 698, 699, 700, 701, 702, 703, 704, 801, 821, 841, 841]
     subAddresses = [ "{},{},{},{},{},{},{}".format(*filterAdresses[index]) for index in falseErrorCodeList]
     
-    print("Length of falseErrorCodes: {}".format(len(falseErrorCodeList)))
+    
     print("Indices of indices 201, 202 and 203 of falseErrorCodeList: {} ; {} ; {}".format(falseErrorCodeList[201], falseErrorCodeList[202], falseErrorCodeList[203]))
     print("Addresses of indices 201, 202 and 203 of falseErrorCodeList: {} ; {} ; {}".format(subAddresses[201],subAddresses[202],subAddresses[203]))
     
@@ -357,9 +367,9 @@ if __name__ == '__main__':
     #subAddressesList =[ filterAdresses[index] for index in falseErrorCodeList]
     #print( subAddresses)
     
-    sys.exit()
+    #sys.exit()
     
-    with open(sys.argv[1]) as infile, open(sys.argv[2], "w") as outfile:
+    with open(sys.argv[2]) as infile, open(sys.argv[3], "w") as outfile:
         subCnt = substituteErrors(infile, outfile, subAddresses)
         
         print("Made {} substitutions out of {}".format(subCnt, len(falseErrorCodeList)))
